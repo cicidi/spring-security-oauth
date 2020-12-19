a811242a025e34eabaa66e7d850993a45632d6df
cicidi <cicidi@gmail.com>
2020-12-19 04:10:43 -0800

    change realm ,clientid  and use role for access control

--- oauth-rest/oauth-resource-server/src/main/java/com/baeldung/resource/spring/SecurityConfig.java
+++ oauth-rest/oauth-resource-server/src/main/java/com/baeldung/resource/spring/SecurityConfig.java
@@ -1,27 +1,51 @@
 package com.baeldung.resource.spring;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+import java.util.jar.JarEntry;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {// @formatter:off
-        http.cors()
-            .and()
-              .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/user/info", "/api/foos/**")
-                  .hasAuthority("SCOPE_read")
-                .antMatchers(HttpMethod.POST, "/api/foos")
-                  .hasAuthority("SCOPE_write")
-                .anyRequest()
-                  .authenticated()
-            .and()
-              .oauth2ResourceServer()
-                .jwt();
-    }//@formatter:on
+  private String jwkSetUri = "http://localhost:8083/auth/realms/electroknox-dev/protocol/openid-connect/certs";
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {// @formatter:off
+    http.cors()
+      .and()
+      .authorizeRequests()
+      .antMatchers(HttpMethod.GET, "/user/info", "/api/foos/**")
+      .hasRole("admin")
+      .antMatchers(HttpMethod.POST, "/api/foos")
+      .hasAnyAuthority("SCOPE_write")
+      .anyRequest()
+      .authenticated()
+      .and()
+      .oauth2ResourceServer(
+        oauth2ResourceServer -> oauth2ResourceServer.jwt(
+          jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+      );
+  }//@formatter:on
+
+  private Converter<Jwt, ? extends AbstractAuthenticationToken> jwtAuthenticationConverter() {
+    JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+    jwtConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRealmRoleConverter());
+    return jwtConverter;
+  }
+
+  @Bean
+  JwtDecoder jwtDecoder() {
+    return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
+  }
 
 }
--- oauth-rest/oauth-resource-server/src/main/java/com/baeldung/resource/web/controller/FooController.java
+++ oauth-rest/oauth-resource-server/src/main/java/com/baeldung/resource/web/controller/FooController.java
@@ -1,5 +1,6 @@
 package com.baeldung.resource.web.controller;
 
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -25,52 +26,53 @@ import com.baeldung.resource.web.dto.FooDto;
 @RequestMapping(value = "/api/foos")
 public class FooController {
 
-    private IFooService fooService;
+  private IFooService fooService;
 
-    public FooController(IFooService fooService) {
-        this.fooService = fooService;
+  public FooController(IFooService fooService) {
+    this.fooService = fooService;
+  }
+
+  @CrossOrigin(origins = "*")
+  @GetMapping(value = "/{id}")
+  public FooDto findOne(@PathVariable Long id, Principal principal) {
+    System.out.println(principal.getName());
+    Foo entity = fooService.findById(id)
+      .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    return convertToDto(entity);
+  }
+
+  @ResponseStatus(HttpStatus.CREATED)
+  @PostMapping
+  public void create(@RequestBody FooDto newFoo) {
+    Foo entity = convertToEntity(newFoo);
+    this.fooService.save(entity);
+  }
+
+  @GetMapping
+  public Collection<FooDto> findAll() {
+    Iterable<Foo> foos = this.fooService.findAll();
+    List<FooDto> fooDtos = new ArrayList<>();
+    foos.forEach(p -> fooDtos.add(convertToDto(p)));
+    return fooDtos;
+  }
+
+  @PutMapping("/{id}")
+  public FooDto updateFoo(@PathVariable("id") Long id, @RequestBody FooDto updatedFoo) {
+    Foo fooEntity = convertToEntity(updatedFoo);
+    return this.convertToDto(this.fooService.save(fooEntity));
+  }
+
+  protected FooDto convertToDto(Foo entity) {
+    FooDto dto = new FooDto(entity.getId(), entity.getName());
+
+    return dto;
+  }
+
+  protected Foo convertToEntity(FooDto dto) {
+    Foo foo = new Foo(dto.getName());
+    if (!StringUtils.isEmpty(dto.getId())) {
+      foo.setId(dto.getId());
     }
-
-    @CrossOrigin(origins = "http://localhost:8089")
-    @GetMapping(value = "/{id}")
-    public FooDto findOne(@PathVariable Long id) {
-        Foo entity = fooService.findById(id)
-            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-        return convertToDto(entity);
-    }
-
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping
-    public void create(@RequestBody FooDto newFoo) {
-        Foo entity = convertToEntity(newFoo);
-        this.fooService.save(entity);
-    }
-
-    @GetMapping
-    public Collection<FooDto> findAll() {
-        Iterable<Foo> foos = this.fooService.findAll();
-        List<FooDto> fooDtos = new ArrayList<>();
-        foos.forEach(p -> fooDtos.add(convertToDto(p)));
-        return fooDtos;
-    }
-
-    @PutMapping("/{id}")
-    public FooDto updateFoo(@PathVariable("id") Long id, @RequestBody FooDto updatedFoo) {
-        Foo fooEntity = convertToEntity(updatedFoo);
-        return this.convertToDto(this.fooService.save(fooEntity));
-    }
-
-    protected FooDto convertToDto(Foo entity) {
-        FooDto dto = new FooDto(entity.getId(), entity.getName());
-
-        return dto;
-    }
-
-    protected Foo convertToEntity(FooDto dto) {
-        Foo foo = new Foo(dto.getName());
-        if (!StringUtils.isEmpty(dto.getId())) {
-            foo.setId(dto.getId());
-        }
-        return foo;
-    }
+    return foo;
+  }
 }
